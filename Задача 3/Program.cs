ec8dc33a04e4dbfb0226d4fd5538363b86f2a50f
efim <efim.krutihin@yandex.ru>
2023-06-06 16:45:31 +0300

    succesful execution of the task3

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,27 +1,24 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-string answer;
+string answer = "";
 int sqrt;
 int num;
 
 do
 {
-    try
-    {
     Console.WriteLine("Введите ваше число: ");
         num = Convert.ToInt32(Console.ReadLine());
 
+        if( num <= 0){
+            Console.WriteLine("введено некорректное значение");
+        } else{
+
         for (int i = 1; i <= num; i++)
         {
-            sqrt = i * 4;
+            sqrt = i * i * i;
             Console.WriteLine(sqrt);
         }
-    }
-    catch (System.Exception)
-    {
-        Console.WriteLine("Ошибка: Введено некорректное значение");
-    }
-
+        }
     Console.WriteLine("Хотите продолжить?");
     answer = Console.ReadLine();
 } while (answer.ToLower() == "yes" || answer.ToLower() == "да");
