a3f6ca30868d099efb3f3f966ff7155b7f2ec933
efim <efim.krutihin@yandex.ru>
2023-06-06 16:15:35 +0300

    add check system.exception in task3

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,18 +1,27 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 string answer;
-
-do{
-
-    Console.WriteLine("Введите ваше число: ");
-int num = Convert.ToInt32(Console.ReadLine());
 int sqrt;
+int num;
 
-for ( int i = 1; i <= num; i++){
-    sqrt = i * 4;
-    Console.WriteLine(sqrt);
-}
+do
+{
+    try
+    {
+    Console.WriteLine("Введите ваше число: ");
+        num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Хотите продолжить?");
-answer = Console.ReadLine();
-}while (answer.ToLower() == "yes" || answer.ToLower() == "да");
+        for (int i = 1; i <= num; i++)
+        {
+            sqrt = i * 4;
+            Console.WriteLine(sqrt);
+        }
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Ошибка: Введено некорректное значение");
+    }
+
+    Console.WriteLine("Хотите продолжить?");
+    answer = Console.ReadLine();
+} while (answer.ToLower() == "yes" || answer.ToLower() == "да");
