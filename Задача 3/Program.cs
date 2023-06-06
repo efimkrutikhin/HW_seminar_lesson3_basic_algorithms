19a32bf60d6ada32ec74e5e0de7e3c6f3a60db4a
efim <efim.krutihin@yandex.ru>
2023-06-06 15:52:28 +0300

    completed task 3

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+string answer;
+
+do{
+
+    Console.WriteLine("Введите ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sqrt;
+
+for ( int i = 1; i <= num; i++){
+    sqrt = i * 4;
+    Console.WriteLine(sqrt);
+}
+
+Console.WriteLine("Хотите продолжить?");
+answer = Console.ReadLine();
+}while (answer.ToLower() == "yes" || answer.ToLower() == "да");
